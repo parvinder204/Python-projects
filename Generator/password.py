1b0cce51de0562ddcee287f3fbf875b99005f38f
Parvinder <22mcc20043@cuchd.in>
2024-04-22 00:37:48 +0530

    Password

---
+++ Generator/password.py
@@ -0,0 +1,18 @@
+import random
+
+print('Password Generator')
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nHere are your passwords:')             
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
